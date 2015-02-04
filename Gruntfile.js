81d10aaadb6edf128bf6aa0b05b7a71440d4d6c6
Ryan Pavlik <ryan@sensics.com>
2015-02-04 14:01:43 -0600

    Move the common json editor stuff into a layout.

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,8 @@ module.exports = function(grunt) {
         options: {
           flatten: true,
           assets: '<%= config.dist %>/assets',
-          layout: '<%= config.src %>/templates/layouts/default.hbs',
+          layoutdir: '<%= config.src %>/templates/layouts/',
+          layout: 'default.hbs',
           data: '<%= config.src %>/data/*.{json,yml}',
           partials: '<%= config.src %>/templates/partials/*.hbs'
         },
